fcace41200184be73b4b6aa8cd80b1889f2ad24e
Alexey Avdeev <aad.jerry@gmail.com>
2013-06-06 16:15:04 +0400

    правка тестов

--- protected/views/tests/index.php
+++ protected/views/tests/index.php
@@ -1,9 +1,9 @@
 <div class="row-fluid">
   <div class="span12" style="text-align:center;">
-      <label for="js-matrix-dim">Количество матриц: <input type="text" id="js-matrix-dim" value="10" maxlength="2" name="matrixSize" style="width: 20px; display: inline;"></label>
+      <label for="js-matrix-count">Количество матриц: <input type="text" id="js-matrix-count" value="10" maxlength="2" name="matrixSize" style="width: 20px; display: inline;"></label>
       <label for="js-matrix-dim">Размерность матриц: <input type="text" id="js-matrix-dim" value="3" maxlength="2" name="matrixSize" style="width: 20px; display: inline;"></label>
-      <label for="js-matrix-dim">Нижняя граница чисел: <input type="text" id="js-matrix-dim" value="1" maxlength="2" name="matrixSize" style="width: 20px; display: inline;"></label>
-      <label for="js-matrix-dim">Верхняя граница чисел: <input type="text" id="js-matrix-dim" value="9" maxlength="2" name="matrixSize" style="width: 20px; display: inline;"></label>
+      <label for="js-matrix-low">Нижняя граница чисел: <input type="text" id="js-matrix-low" value="1" maxlength="2" name="matrixSize" style="width: 20px; display: inline;"></label>
+      <label for="js-matrix-high">Верхняя граница чисел: <input type="text" id="js-matrix-high" value="9" maxlength="2" name="matrixSize" style="width: 20px; display: inline;"></label>
       <label for="js-test-empty-count">Процент пустых клеток: <input type="text" id="js-test-empty-count" value="0" maxlength="2" name="matrixSize" style="width: 20px; display: inline;">%</label>
       <input class="btn btn-primary" id="js-matrix-gen" value="Запустить тесты" type="submit">
   </div>
